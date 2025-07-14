eda5d5b5e7eb27baa7e426889d960aaee65b77af
phu-m4 <phu-m4@phu-m4s-MacBook-Pro.local>
2025-07-14 14:36:32 +0700

    chore: next config, prettier config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // สำหรับ GitHub Pages
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
 };
 
 export default nextConfig;
