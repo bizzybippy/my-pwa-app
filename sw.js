d5626bd80518c90e7d389b9ed64affbffc58b741
bizzybippy <bizzybippy@users.noreply.github.com>
2025-07-14 10:20:14 +0000

    deploy: 5e3c18d781473782392084dcab861739b4c8d4dd

--- sw.js
+++ sw.js
@@ -8,17 +8,17 @@ const urlsToCache = [
   `${BASE_PATH}/assets/icon-512x512.png`,
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache).catch((error) => {
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache).catch(error => {
         console.error('Service Worker: Cache failed', error);
       });
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   const requestUrl = new URL(event.request.url);
   if (requestUrl.pathname.startsWith(BASE_PATH)) {
     // แคชไฟล์ static จาก _next/static/ และ .html
@@ -27,12 +27,12 @@ self.addEventListener('fetch', (event) => {
       requestUrl.pathname.endsWith('.html')
     ) {
       event.respondWith(
-        caches.open(CACHE_NAME).then((cache) => {
-          return cache.match(event.request).then((response) => {
+        caches.open(CACHE_NAME).then(cache => {
+          return cache.match(event.request).then(response => {
             return (
               response ||
               fetch(event.request)
-                .then((networkResponse) => {
+                .then(networkResponse => {
                   cache.put(event.request, networkResponse.clone());
                   return networkResponse;
                 })
@@ -53,10 +53,10 @@ self.addEventListener('fetch', (event) => {
     } else {
       // สำหรับการร้องขออื่นๆ (เช่น API) ลองใช้แคชก่อน ถ้าไม่มีให้ fetch
       event.respondWith(
-        caches.match(event.request).then((response) => {
+        caches.match(event.request).then(response => {
           return (
             response ||
-            fetch(event.request).catch((error) => {
+            fetch(event.request).catch(error => {
               console.error('Service Worker: Fetch failed', error);
             })
           );
@@ -66,13 +66,13 @@ self.addEventListener('fetch', (event) => {
   }
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
       );
     })
   );
