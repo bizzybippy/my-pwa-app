a0da44adfe34e1c061572a0e6f4f4f1c92418e6a
phu-m4 <phu-m4@phu-m4s-MacBook-Pro.local>
2025-07-14 16:23:04 +0700

    fix: pwa config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    pwa: {
-      dest: 'public',
-      register: true,
-      skipWaiting: true,
-    },
-  };
-  
-  export default nextConfig;
+  output: 'export',
+  basePath: isProd ? '/my-pwa-app' : '',
+  assetPrefix: isProd ? '/my-pwa-app/' : '',
+  reactStrictMode: true,
+  trailingSlash: true,
+};
+
+export default nextConfig;
