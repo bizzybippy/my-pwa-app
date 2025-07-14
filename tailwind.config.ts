8d35c1dc403d0d2ecd34802d0c6d83e5ad420be4
phu-m4 <phu-m4@phu-m4s-MacBook-Pro.local>
2025-07-14 13:52:51 +0700

    chore: install package

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsl(var(--primary) / <alpha-value>)",
+        secondary: "hsl(var(--secondary) / <alpha-value>)",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
